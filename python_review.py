fc3e2cbdb4eb00fdc6d012a61ee8a6d276a21f73
apple <apple@Randalls-MacBook-Pro.local>
2025-04-10 09:37:02 -0400

    level 4 complete

--- python_review.py
+++ python_review.py
@@ -2,3 +2,15 @@
 
 print("hello, World")
 
+x = None
+
+example = "I'm a string" # The variable called example is the value "I'm a string"
+a = 3 # The variable called a is now holding the integer value 3
+b = 4.0 # The variable called b is now bolding the float value 4.0
+c = True # The variable called c is now holding the bolean value True
+d = False # The variable called d is now holding the bolean value False
+e ="Hey!" ##The variable called e is now holding the string
+f = None #The variable called f is now holding the none value none
+age = 32 # The variable called age is now holding the integer value 32
+name = "Insert your instrument here" # The variable called name is the value "Insert your instrument here"
+intrument = "Insert your instrument here" # The variable called instrument is the value "Insert your instrument here"
